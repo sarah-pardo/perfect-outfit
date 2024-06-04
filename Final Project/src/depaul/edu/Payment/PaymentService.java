85365ab145915b2443e4586d52233b7df6afbfa9
sarah-pardo <spardo2@wellesley.edu>
2024-06-04 18:02:36 -0500

    Update PaymentService.java

--- Final Project/src/depaul/edu/Payment/PaymentService.java
+++ Final Project/src/depaul/edu/Payment/PaymentService.java
@@ -14,7 +14,7 @@ public class PaymentService {
 		this.inputHelper = inputHelper;
 		this.cartPersistenceService = cartPersistenceService;
 	}
-	
+
 	public void checkoutConfirmation(String username, Cart cart) {
         int option = inputHelper.getIntFromStringInput("Enter 1 to confirm cart and proceed to checkout.\n"
         		+ "Enter 0 to go back.");
@@ -32,13 +32,14 @@ public class PaymentService {
 	}
 	public void processOrder(String username, Cart cart) {
 		Predicate<String> containsOnlyLettersAndPunctuation = s -> s.matches("[a-zA-Z., ]+");
-		Predicate<String> isSixteenDigitNumber = s -> s.replaceAll("\\s", "").matches("\\d{16}");
+		Predicate<String> isSixteenDigitNumber = s -> s.trim().replaceAll("\\s", "").matches("\\d{16}");
 		System.out.println("Follow the prompts to enter your shipping address:");
 		int streetNumber = inputHelper.getIntFromStringInput("Street number:");
 		String streetName = inputHelper.getStringInput("Street name:", containsOnlyLettersAndPunctuation);
 		String city = inputHelper.getStringInput("City:", containsOnlyLettersAndPunctuation);
 		String state = inputHelper.getStringInput("State:", containsOnlyLettersAndPunctuation);
 		String creditCard = inputHelper.getStringInput("Please enter your 16 digit credit card number:", isSixteenDigitNumber);
+		creditCard = creditCard.trim().replaceAll("\\s", "");
 		System.out.println("Your order is on its way! Here is the summary:");
 		System.out.println(username + "'s order:");
 		System.out.println(streetNumber + " " + streetName + "\n" + city + ", " + state);
